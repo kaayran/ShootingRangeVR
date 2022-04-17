d24b337c7228ff889a1e64fea7cc2f192fa0f35d
kaayran <dodororo132@gmail.com>
2022-04-17 20:17:31 +0300

    change mass & drag of bullets, add bulletSpeedLogger for each bullet to check their speed in console

---
+++ Assets/Scripts/SpeedObserver/BulletSpeedLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpeedObserver
+{
+    public class BulletSpeedLogger : MonoBehaviour
+    {
+        private Rigidbody _bulletRigidbody;
+
+        private void Start()
+        {
+            _bulletRigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            Debug.Log(_bulletRigidbody.velocity);
+        }
+    }
+}
