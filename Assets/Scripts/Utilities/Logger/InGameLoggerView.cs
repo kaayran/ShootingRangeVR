55182749f15c43b0225b0f55d987a8fcd47ae28f
kaayran <dodororo132@gmail.com>
2022-04-30 14:32:16 +0300

    define base methods and Actions

--- Assets/Scripts/Utilities/Logger/InGameLoggerView.cs
+++ Assets/Scripts/Utilities/Logger/InGameLoggerView.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace Utilities.Logger
@@ -6,5 +7,30 @@ namespace Utilities.Logger
     [RequireComponent(typeof(TextMeshProUGUI))]
     public class InGameLoggerView : Component
     {
+        private TextMeshProUGUI _text;
+
+        public void Init()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            InGameLogger.Clear += OnClearLog;
+            InGameLogger.DataSend += OnDataSend;
+        }
+
+        private void OnDataSend(string message, bool append)
+        {
+            if (append)
+            {
+                _text.text += "\n" + message;
+            }
+            else
+            {
+                _text.text = message;
+            }
+        }
+
+        private void OnClearLog()
+        {
+            _text.text = string.Empty;
+        }
     }
 }
