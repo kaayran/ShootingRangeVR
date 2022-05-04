3b08448fde287c49f0a625cf1d605fc6d5c04386
kaayran <dodororo132@gmail.com>
2022-05-05 00:22:31 +0300

    change collision ignoring to GetComponentsInChildren

--- Assets/Scripts/StructureComponents/CollisionIgnoring.cs
+++ Assets/Scripts/StructureComponents/CollisionIgnoring.cs
@@ -5,10 +5,12 @@ namespace StructureComponents
 {
     public class CollisionIgnoring : MonoBehaviour
     {
-        [SerializeField] private List<Collider> _colliders;
+        private List<Collider> _colliders;
 
         public void Init()
         {
+            _colliders = new List<Collider>(GetComponentsInChildren<Collider>());
+
             for (var i = 0; i < _colliders.Count - 1; i++)
                 _colliders.GetRange(i + 1, _colliders.Count - i - 1).ForEach(col =>
                     Physics.IgnoreCollision(_colliders[i], col));
