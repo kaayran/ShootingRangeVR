c2683faf5b78f44f78cee23f09c4c597d35a51d1
kaayran <dodororo132@gmail.com>
2022-05-21 10:44:27 +0300

    added bakery asset, generated test lightdata

---
+++ Assets/Scripts/Utilities/CombineSubMeshes.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class CombineSubMeshes : MonoBehaviour
+{
+    [ContextMenu("Combine Meshes")]
+    private void CombineMeshes()
+    {
+        var meshFilters = GetComponentsInChildren<MeshFilter>();
+        var combine = new CombineInstance[meshFilters.Length];
+
+        for (var i = 0; i < meshFilters.Length; i++)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+        }
+
+        var mesh = new Mesh
+        {
+            name = "Floor"
+        };
+        
+        mesh.CombineMeshes(combine);
+        Unwrapping.GenerateSecondaryUVSet(mesh);
+        
+        transform.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.SetActive(true);
+    }
+}
