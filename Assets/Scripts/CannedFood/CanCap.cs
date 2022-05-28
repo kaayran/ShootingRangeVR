661d7f2316525325f1cea853bd3255038fb68ee0
kaayran <dodororo132@gmail.com>
2022-05-28 22:46:57 +0300

    added food cans, add gameplay mechanis to open it

---
+++ Assets/Scripts/CannedFood/CanCap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace CannedFood
+{
+    public class CanCap : MonoBehaviour
+    {
+        [SerializeField] private int _takesToOpen = 3;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<KnifeRazor>(out _)) return;
+            if (_takesToOpen-- == 1) return;
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CannedFood/KnifeRazor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CannedFood
+{
+    public class KnifeRazor : MonoBehaviour
+    {
+        
+    }
+}
