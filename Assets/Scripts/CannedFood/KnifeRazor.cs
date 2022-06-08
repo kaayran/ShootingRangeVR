86e5c0e9e800d294bfc10f374524b807dd85869c
kaayran <dodororo132@gmail.com>
2022-06-08 18:56:43 +0300

    fixed sounds, removed knife throwing

--- Assets/Scripts/CannedFood/KnifeRazor.cs
+++ Assets/Scripts/CannedFood/KnifeRazor.cs
@@ -1,32 +1,8 @@
-﻿using System;
-using JetBrains.Annotations;
-using UnityEngine;
-using Valve.VR.InteractionSystem;
+﻿using UnityEngine;
 
 namespace CannedFood
 {
     public class KnifeRazor : MonoBehaviour
     {
-        private Rigidbody _rb;
-
-        private void Start()
-        {
-            _rb = GetComponent<Rigidbody>();
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (_rb.velocity.magnitude < 25f) return;
-            var contact = collision.contacts[0];
-            if (Vector3.Dot(contact.normal, transform.forward) > -0.5f) return;
-
-            _rb.isKinematic = true;
-        }
-
-        [UsedImplicitly]
-        private void OnAttachedToHand(Hand hand)
-        {
-            _rb.isKinematic = false;
-        }
     }
 }
